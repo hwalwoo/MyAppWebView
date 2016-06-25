fc28fbab1c67c57250a812da21ba6ecd9fd1ad8b
hwalwoo <hwalwoo@naver.com>
2016-06-25 12:27:21 +0900

    웹뷰에서 네이버 보기

---
+++ app/src/main/java/com/example/user/myappwebview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.user.myappwebview;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.naver.com");
+    }
+}
